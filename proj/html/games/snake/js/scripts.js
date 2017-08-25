2b66d73a76b88508c6802312c5645a55d1e7075e
code bender <bhagwati@thekha.in>
2017-08-25 16:53:44 +0100

    completed basic snake game

--- proj/html/games/snake/js/scripts.js
+++ proj/html/games/snake/js/scripts.js
@@ -10,6 +10,7 @@ if(!window.cancelAnimationFrame) {
 //Constants
 UP = 0; RIGHT = 1; DOWN = 2; LEFT = 3;
 EMPTY = 0; FOOD = 1; SNAKE = 2;
+KEY_LEFT = 37; KEY_UP = 38; KEY_RIGHT = 39; KEY_DOWN = 40;
 
 //Config
 rows = 25; cols = 25; cellSize = 20;
@@ -44,7 +45,7 @@ snake = {
 		this.insert(x, y);
 	},
 	insert: function(r, c) {
-		this._snake.push({x: c, y: r});
+		this._snake.unshift({x: c, y: r});
 		this.last = this._snake[0];
 	},
 	remove: function() {
@@ -68,6 +69,7 @@ food = {
 }
 //Game objects
 var canvas, ctx, keystate, frames;
+var score = 0;
 
 //Game functions
 function main() {
@@ -77,7 +79,14 @@ function main() {
 	ctx = canvas.getContext("2d");
 
 	frames = 0;
-	keystate = {};
+	keyState = {};
+
+	document.addEventListener("keydown", function(evt) {
+		keyState[evt.keyCode] = true;
+	});
+	document.addEventListener("keyup", function(evt) {
+		delete keyState[evt.keyCode];
+	});
 
 	init();
 	loop();
@@ -101,25 +110,49 @@ function loop() {
 
 function update() {
 	frames++;
-	if((frames % 10) == 0) {
+	if((frames % 5) == 0) {
+		if(keyState[KEY_LEFT] && (snake.direction != RIGHT)) snake.direction = LEFT;
+		if(keyState[KEY_UP] && (snake.direction != DOWN)) snake.direction = UP;
+		if(keyState[KEY_RIGHT] && (snake.direction != LEFT)) snake.direction = RIGHT;
+		if(keyState[KEY_DOWN] && (snake.direction != UP)) snake.direction = DOWN;
+		var nx = snake.last.x;
+		var ny = snake.last.y;
 		switch(snake.direction) {
 			case UP:
+				nx--;
 				break;
 			case RIGHT:
+				ny++;
 				break;
 			case DOWN:
+				nx++;
 				break;
 			case LEFT:
+				ny--;
 				break;
 		}
-		
+		if(nx < 0 || nx > (rows - 1) || (ny < 0) || (ny > rows - 1)) {
+			return init();
+		}
+		if(board.get(ny, nx) === FOOD) {
+			var tail = {x: nx, y: ny};
+			food.set();
+			score++;
+			console.log(score);
+		} else {
+			var tail = snake.remove();
+			board.set(EMPTY, tail.y, tail.x);
+			tail.x = nx;
+			tail.y = ny;
+		}
+		board.set(SNAKE, tail.y, tail.x);
+		snake.insert(tail.y, tail.x);
 	}
 }
 
 function draw() {
 	for(i = 0; i < cols; i++) {
 		for(j = 0; j < rows; j++) {
-
 			switch(board.get(i, j)) {
 				case EMPTY:
 					ctx.fillStyle = "#fff";
